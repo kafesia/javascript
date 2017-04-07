a544421595f2059ee65c00ec6d2d6c46c0d297af
Kafesia <kafesia77@gmail.com>
2017-04-08 05:33:20 +0700

    Update defer-kf.js

--- defer-kf.js
+++ defer-kf.js
@@ -1,3 +1,11 @@
+/*! buttons v1.1.4 - 2016-05-01 | Copyright (c) 2016 Adam Bouqdib (http://abemedia.co.uk) | donreach.com/license */
+ (function() {
+    var dr = document.createElement('script');
+    dr.type = 'text/javascript'; dr.async = true;
+    dr.src = '//share.donreach.com/buttons.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dr);
+  })();
+
 /* By Osvaldas Valutis, www.osvaldas.info   Available for use under the MIT License */
 ;
 (function(e, t, n, r) {
